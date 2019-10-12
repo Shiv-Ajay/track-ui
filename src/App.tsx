92bc9917eb7ad119d51496146e0a7e2690d08e8c
Abhinav <rajpurohitabhinav@gmail.com>
2019-10-12 22:34:49 +0530

    Committing header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './App.scss';
-import { Header } from './components/Header/Header';
+import Header from './components/Header/Header';
 
 class App extends React.Component {
   render() {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,3 +14,4 @@ export class Header extends Component {
     }
 }
 
+export default Header;
