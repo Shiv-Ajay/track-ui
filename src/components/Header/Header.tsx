602f0e4ee8aa153b99684e2ef64c2fd32abe4cd4
Abhinav <rajpurohitabhinav@gmail.com>
2019-10-13 01:43:37 +0530

    image added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
+import logo from '../../assets/images/bikaner.jpeg';
 import './Header.scss';
 
 export class Header extends React.Component {
     render() {
         return (
-            <header className="App-header p-4">
-                <div className="row">
-                    <div className="col">
-                        <img src="src/assets/images/bikaner.jpeg" alt="Main Logo"/>
+            <header className="App-header p-2">
+                <div className="header row d-flex">
+                    <div className="header-logo col">
+                        <img src={logo} alt="Main Logo"/>
+                        <span>Sweet Home</span>
                     </div>
-                    <div className="col">Worlddddddddddd</div>
+                    <div className="col ml-auto">Worlddddddddddd</div>
                 </div>
             </header>
         )
