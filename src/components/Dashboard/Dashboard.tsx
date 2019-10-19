8e4f5098b69038f8f3b5d7300446d98de347beae
Abhinav <rajpurohitabhinav@gmail.com>
2019-10-19 21:37:00 +0530

    added interface and dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,57 @@
 import * as React from 'react';
 import './Dashboard.scss';
 
-export class Dashboard extends React.Component {
+interface DashboardProps {
+    name: String;
+    age?: Number;
+}
+
+interface DashboardState {
+    address: String;
+    phone?: Number;
+}
+
+export class Dashboard extends React.Component<DashboardProps, DashboardState> {
     render() {
         return (
-            <div>
-                
+            <div className="dashboard-container">
+                <div className="row mt-3">
+                    <div className="col-lg-6 col-md-6 left-widgets p-3">
+                        <div className="personal-document">
+                            Here i will publish list of all publish document which is accessible throughout all views.
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-md-6 right-widgets p-3">
+                        <div className="credit-card">
+                            Here i will publish list of all credit cards which is accessible throughout all views.
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-3">
+                    <div className="col-lg-6 col-md-6 left-widgets p-3 mt-3">
+                        <div className="bank-account">
+                            Here i will publish list of all bank documents which is accessible throughout all views.  
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-md-6 right-widgets p-3 mt-3">
+                        <div className="daily-expense">
+                            Here i will publish list of all daily expenses which is accessible throughout all views.
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-3">
+                    <div className="col-lg-6 col-md-6 left-widgets p-3 mt-3">
+                        <div className="bill-management">
+                            Here i will publish list of all bills which is accessible throughout all views.
+                        </div>
+                    </div>
+                    
+                    <div className="col-lg-6 col-md-6 right-widgets p-3 mt-3">
+                        <div className="vehicle-management">
+                            Here i will publish list of all vehicles which is accessible throughout all views.
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
